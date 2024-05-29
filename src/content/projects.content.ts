78f37ec3f3baa7ae236bea3ee873b5db84c93073
Iván Ruiz <ivr.644@gmail.com>
2024-05-30 00:26:34 +0200

    Initial commit

---
+++ src/content/projects.content.ts
@@ -0,0 +1,20 @@
+export const ProjectsContent = [
+  {
+    ID: "#glint-blog",
+    NAME: "Glint Blog",
+    DESCRIPTION:
+      "A space specialized in web development, UX and accessibility. Find articles and resources to improve your skills in these areas.",
+    PAGE_URL: "https://glint-blog.com",
+    GITHUB_REPO: "https://github.com/Ivanruii/glint-blog",
+    PERSONAL_PROJECT: true,
+  },
+  {
+    ID: "#mogno-modeler",
+    NAME: "Mongo Modeler",
+    DESCRIPTION:
+      "Mongo Modeler is a free, open source tool for visualization of MongoDB schema design.",
+    PAGE_URL: "https://mongomodeler.com/",
+    GITHUB_REPO: "https://github.com/Lemoncode/mongo-modeler",
+    PERSONAL_PROJECT: false,
+  },
+];
