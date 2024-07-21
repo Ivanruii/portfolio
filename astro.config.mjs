19606ef1bca04d0ed0e4d82a1f1d45156d641f77
Iván Ruiz <ivr.644@gmail.com>
2024-07-21 14:00:35 +0200

    Updated imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import preact from "@astrojs/preact";
-import path from "path";
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "static",
   adapter: vercel(),
-  vite: {
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-      },
-    },
-  },
 });
